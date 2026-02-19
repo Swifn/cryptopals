13489a6fcec597f17264ef99deac5dc66aa14dd7
Anthony Badpegga <aib924@student.bham.ac.uk>
2026-02-19 19:34:43 +0000

    FIX: bytes_to_base64() type alignment

--- src/sets/set1/challenge1.rs
+++ src/sets/set1/challenge1.rs
@@ -4,7 +4,7 @@ use crate::utils::encoding::hex_to_bytes;
 pub fn run() -> String {
     let hex_string: &str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let byte_vec: Vec<u8> = hex_to_bytes(hex_string);
-    let base64_string: String = bytes_to_base64(byte_vec);
+    let base64_string: String = bytes_to_base64(&byte_vec);
 
     base64_string
 }
