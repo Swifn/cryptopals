623bba061e82a5a186e2d8cd239b965a99d60465
Anthony Badpegga <aib924@student.bham.ac.uk>
2026-02-19 18:25:51 +0000

    ADD: challenge1 answer

--- src/sets/set1/challenge1.rs
+++ src/sets/set1/challenge1.rs
@@ -1,11 +1,12 @@
+use crate::utils::encoding::bytes_to_base64;
 use crate::utils::encoding::hex_to_bytes;
+
 pub fn run() -> String {
-    let s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    let result = hex_to_bytes(s);
+    let hex_string: &str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let byte_vec: Vec<u8> = hex_to_bytes(hex_string);
+    let base64_string: String = bytes_to_base64(byte_vec);
 
-    let res = (String::from_utf8(result)).unwrap();
-
-    res
+    base64_string
 }
 
 #[cfg(test)]
