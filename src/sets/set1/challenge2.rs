d375074db6e3cdaf989dd6067016772f72557843
Anthony Badpegga <aib924@student.bham.ac.uk>
2026-02-19 19:35:13 +0000

    ADD: init set1 challenge2

--- src/sets/set1/challenge2.rs
+++ src/sets/set1/challenge2.rs
@@ -1,1 +1,20 @@
 
+pub fn run() -> String {
+
+}
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn challenge_correct() {
+        let result = run();
+        assert_eq!(
+            result,
+            "746865206b696420646f6e277420706c6179"
+        )
+    }
+
+}
