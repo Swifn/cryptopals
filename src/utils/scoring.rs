c281ba4716915d138d5ffc7dd5d0d2f463bf7fc8
Anthony Badpegga <aib924@student.bham.ac.uk>
2026-02-20 20:52:50 +0000

    FIX: Scoring to +3 for ' ', not '_'.
    Converted chars to lowercase for more accurate scoring.
    
    c -> c:char in map

--- src/utils/scoring.rs
+++ src/utils/scoring.rs
@@ -1,9 +1,9 @@
 pub fn scoring(s: &str) -> i32 {
     s.chars()
-        .map(|c| match c {
+        .map(|c: char| match c.to_ascii_lowercase() {
             'e' | 't' | 'a' | 'o' | 'i' => 2,
             'a'..='z' => 1,
-            '_' => 3,
+            ' ' => 3,
             _ => -1,
         })
         .sum()
