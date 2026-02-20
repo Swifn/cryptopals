e4acc63525693a5625fe270a39b3ececacef815b
Anthony Badpegga <aib924@student.bham.ac.uk>
2026-02-20 20:34:56 +0000

    ADD: pub fn scoring

--- src/utils/scoring.rs
+++ src/utils/scoring.rs
@@ -1,1 +1,9 @@
+pub fn scoring(s: &str) -> i32 {
 
+    s.chars().map(|c:char | match c {
+        'e' | 't' | 'a' | 'o' | 'i' => 2,
+        'a'..='z' => 1,
+        '_' => 3,
+        _ => 1,
+    }).sum()
+}
