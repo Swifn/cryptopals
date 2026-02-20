c3880b08aebaa0f0da3120c00afac3b053f4e3df
Anthony Badpegga <aib924@student.bham.ac.uk>
2026-02-20 20:34:41 +0000

    ADD: pub fn single_byte_xor_cipher, struct Score, idiomatically wrote pub fn fixed_xor

--- src/utils/xor.rs
+++ src/utils/xor.rs
@@ -1,9 +1,33 @@
-pub fn fixed_xor(bytes: &[u8], key: &[u8]) -> Vec<u8> {
-    let mut xor: Vec<u8> = Vec::new();
+use crate::utils::scoring::scoring;
 
-    for i in 0..bytes.len() {
-        xor.push(bytes[i] ^ key[i])
+pub struct Score {
+    pub text: String,
+    pub key: u8,
+    pub score: i32,
+}
+
+pub fn fixed_xor(bytes: &[u8], key: &[u8]) -> Vec<u8> {
+
+    bytes.iter().zip(key).map(|(&b, &k)| b ^ k ).collect()
+
+}
+
+pub fn single_byte_xor_cipher(bytes: &[u8]) -> Score {
+    let mut best: Option<Score> = None;
+
+    for key in 0u8..=255u8 {
+
+        let xor: Vec<u8> = bytes.iter().map(|&b| b ^ key).collect();
+
+        if let Ok(text) = String::from_utf8(xor){
+            let sc: i32 = scoring(&text);
+            let candidate = Score {text, key, score: sc};
+
+            if best.as_ref().map_or(true, | b| candidate.score > b.score){
+                best = Some(candidate)
+            }
+        }
     }
 
-    xor
+    best.unwrap()
 }
