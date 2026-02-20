19666c11f84b5c5cf1aaf7708d0f0ce64923d581
Anthony Badpegga <aib924@student.bham.ac.uk>
2026-02-20 16:13:35 +0000

    FIX: cargo fmt

--- src/utils/xor.rs
+++ src/utils/xor.rs
@@ -1,7 +1,7 @@
-pub fn fixed_xor (bytes: &[u8], key: &[u8]) -> Vec<u8> {
+pub fn fixed_xor(bytes: &[u8], key: &[u8]) -> Vec<u8> {
     let mut xor: Vec<u8> = Vec::new();
-    
-    for i in 0..bytes.len(){
+
+    for i in 0..bytes.len() {
         xor.push(bytes[i] ^ key[i])
     }
 
