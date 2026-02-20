4bfe7a8ba53be15373e777877b3b5a2b790a6897
Anthony Badpegga <aib924@student.bham.ac.uk>
2026-02-20 20:45:07 +0000

    FIX: scoring calculation for any other value: 1 -> -1

--- src/utils/scoring.rs
+++ src/utils/scoring.rs
@@ -1,9 +1,9 @@
 pub fn scoring(s: &str) -> i32 {
 
-    s.chars().map(|c:char | match c {
+    s.chars().map(|c | match c {
         'e' | 't' | 'a' | 'o' | 'i' => 2,
         'a'..='z' => 1,
         '_' => 3,
-        _ => 1,
+        _ => -1,
     }).sum()
 }
