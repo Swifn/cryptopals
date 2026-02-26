be43d68d39071fa97d7b5952fa72b535e36829e4
Anthony Badpegga <aib924@student.bham.ac.uk>
2026-02-26 16:05:06 +0000

    ADD: enable set1_challenge4

--- src/sets/mod.rs
+++ src/sets/mod.rs
@@ -10,7 +10,7 @@ pub fn get_challenges() -> HashMap<(&'static str, &'static str), ChallengeFn> {
     map.insert(("set1", "1"), set1::challenge1::run);
     map.insert(("set1", "2"), set1::challenge2::run);
     map.insert(("set1", "3"), set1::challenge3::run);
-    // map.insert(("set1", "4"), set1::challenge4::run);
+    map.insert(("set1", "4"), set1::challenge4::run);
     // map.insert(("set1", "5"), set1::challenge5::run);
 
     map
