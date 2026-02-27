d2749759daa6acad63d84a5395ded4bd6fa33d2b
Anthony Badpegga <aib924@student.bham.ac.uk>
2026-02-27 19:29:42 +0000

    ADD: set1_challenge5 answer

--- src/sets/set1/challenge5.rs
+++ src/sets/set1/challenge5.rs
@@ -1,1 +1,25 @@
+use crate::utils::encoding::bytes_to_hex;
+use crate::utils::xor::repeating_key_xor;
 
+pub fn run() -> String {
+    let s: &str = "Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal";
+
+    let hex = bytes_to_hex(&repeating_key_xor(s.as_bytes(), b"ICE"));
+
+    hex
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn challenge_correct() {
+        let result: String = run();
+        assert_eq!(
+            result,
+            "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+        )
+    }
+}
